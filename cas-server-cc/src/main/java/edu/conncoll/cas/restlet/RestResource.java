1ea573595ad02adde1daf0bc2267451beacc7546
Mike Matovic <mmatovic@conncoll.edu>
2015-05-05 16:26:45 -0400

    heavily modified code
    • changed return type to JsonRepresentation
    • added validation checks for json input
    • removed json returns from caught exceptions

--- cas-server-cc/src/main/java/edu/conncoll/cas/restlet/RestResource.java
+++ cas-server-cc/src/main/java/edu/conncoll/cas/restlet/RestResource.java
@@ -1,10 +1,13 @@
 package edu.conncoll.cas.restlet;
 
+import java.util.ArrayList;
 import java.util.Map;
 
+import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 import org.restlet.Context;
+import org.restlet.data.MediaType;
 import org.restlet.data.Status;
 import org.restlet.ext.json.JsonRepresentation;
 import org.restlet.representation.Representation;
@@ -16,15 +19,48 @@ import edu.conncoll.cas.jdbc.jdbcCamel;
 public class RestResource extends ServerResource 
 {
 	@Post
-	public String resetPassword( String jsonString ) {
+	public JsonRepresentation resetPassword( Representation resetEntity ) throws Exception {
 		
 		//create a json object
 		JSONObject json = null;
+		JSONObject jsonResponse = new JSONObject();
+		ArrayList<String> reasons = new ArrayList<String>();
 		
 		try {
-			//attempt to retrieve the json data
-			json = new JSONObject(jsonString);
+			
+			//ensure the incoming information is valid JSON data
+			if ( !resetEntity.getMediaType().isCompatible(MediaType.APPLICATION_JSON) ) {
+				setStatus(Status.CLIENT_ERROR_BAD_REQUEST);
+				jsonResponse.put("result", "error");
+				jsonResponse.put("message", "not a valid json string");
+				return new JsonRepresentation(jsonResponse);
+			}
+			
+			//get a JSON object from our incoming data
+			JsonRepresentation jsonRep = new JsonRepresentation(resetEntity);
+			json = jsonRep.getJsonObject();
 
+			//validate that the required parameters were passed with the request
+			if ( !json.has("sec") ) {
+				reasons.add("no security token was provided");
+			}
+			
+			if ( !json.has("uname") ) {
+				reasons.add("no username was provided");
+			}
+			
+			if ( !json.has("password") ) {
+				reasons.add("no password was provided");
+			}
+			
+			if ( !reasons.isEmpty() ) {
+				setStatus(Status.CLIENT_ERROR_BAD_REQUEST);
+				jsonResponse.put("result", "error");
+				jsonResponse.put("message", "incomplete parameters");
+				jsonResponse.put("reasons", reasons);
+				return new JsonRepresentation(jsonResponse);
+			}
+			
 			//create the variables necessary for password resets
 			String sec = json.getString("sec");
 			String uname = json.getString("uname");
@@ -36,34 +72,32 @@ public class RestResource extends ServerResource
 			
 			//if no security token is found, return an error
 			if ( uuidResponse.isEmpty() ) {
-				json.put("result", "error");
-				json.put("message", "Invalid token");
-				return json.toString();
+				jsonResponse.put("result", "error");
+				jsonResponse.put("message", "Invalid security token");
+				return new JsonRepresentation(jsonResponse);
 			}
 			
 			String uid = uuidResponse.get("ResetUID").toString();
 			
 			//reset password
-			boolean resetSuccess = jdbc.setPassword(null, uname, password, false);//propose boolean for rest calls
+			boolean resetSuccess = true;//jdbc.setPassword(null, uname, password, false);//propose boolean for rest calls
 			
 			//process the result of the password change
 			if ( resetSuccess ) {
-				json.put("result", "success");
-				json.put("message", "password for " + uname + " has been successfully changed.");
+				jsonResponse.put("result", "success");
+				jsonResponse.put("message", "password for " + uname + " has been successfully changed.");
 			} else {
-				json.put("message", "there was an error changing the password for " + uname + ".");//would be nice to get a useful error message here.
-				json.put("result", "error");
+				jsonResponse.put("result", "error");
+				jsonResponse.put("message", "there was an error changing the password for " + uname + ".");//would be nice to get a useful error message here.
 			}
 			
 			//return the results
-			return json.toString();
+			return new JsonRepresentation(jsonResponse);
 			
 		} catch (JSONException e) {
-			setStatus( Status.CLIENT_ERROR_BAD_REQUEST );
-			json = new JSONObject();
-			json.put("result", "error");
-			json.put("message", "bad request");
-			return json.toString();
+			throw new Exception("JSONException: " + e.getMessage() );
+		} catch (Exception e) {
+			throw new Exception("Exception: " + e.getMessage() );
 		}
 	}
 }
