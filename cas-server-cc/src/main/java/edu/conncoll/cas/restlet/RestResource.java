c54fec3f8288e3579d40ffe485c7846eb04868a6
Mike Matovic <mmatovic@conncoll.edu>
2015-05-05 13:48:19 -0400

    updated method

--- cas-server-cc/src/main/java/edu/conncoll/cas/restlet/RestResource.java
+++ cas-server-cc/src/main/java/edu/conncoll/cas/restlet/RestResource.java
@@ -16,14 +16,14 @@ import edu.conncoll.cas.jdbc.jdbcCamel;
 public class RestResource extends ServerResource 
 {
 	@Post
-	public String resetPassword( JsonRepresentation resetEntity ) {
+	public String resetPassword( String jsonString ) {
 		
 		//create a json object
 		JSONObject json = null;
 		
 		try {
 			//attempt to retrieve the json data
-			json = resetEntity.getJsonObject();
+			json = new JSONObject(jsonString);
 
 			//create the variables necessary for password resets
 			String sec = json.getString("sec");
@@ -43,13 +43,27 @@ public class RestResource extends ServerResource
 			
 			String uid = uuidResponse.get("ResetUID").toString();
 			
-			Context context = Context.getCurrent();
-			
 			//reset password
+			boolean resetSuccess = jdbc.setPassword(null, uname, password, false);//propose boolean for rest calls
+			
+			//process the result of the password change
+			if ( resetSuccess ) {
+				json.put("result", "success");
+				json.put("message", "password for " + uname + " has been successfully changed.");
+			} else {
+				json.put("message", "there was an error changing the password for " + uname + ".");//would be nice to get a useful error message here.
+				json.put("result", "error");
+			}
+			
+			//return the results
+			return json.toString();
 			
 		} catch (JSONException e) {
 			setStatus( Status.CLIENT_ERROR_BAD_REQUEST );
+			json = new JSONObject();
+			json.put("result", "error");
+			json.put("message", "bad request");
+			return json.toString();
 		}
-		return null;
 	}
 }
