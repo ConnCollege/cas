c524bdeb5861961e4fcd503693a0e1b96db9efdb
Andrew Tillinghast <atilling@conncoll.edu>
2015-06-09 14:45:30 -0400

    Fixed more typos in login-webflow. Made changes to Checking for Interrupt to support internal SSO

--- cas-server-interrupts/src/main/java/org/jasig/cas/web/flow/CheckInt.java
+++ cas-server-interrupts/src/main/java/org/jasig/cas/web/flow/CheckInt.java
@@ -3,11 +3,14 @@ package org.jasig.cas.web.flow;
 import java.util.List;
 
 import javax.servlet.http.HttpServletRequest;
-
+import javax.validation.constraints.NotNull;
 
 import org.springframework.webflow.execution.RequestContext;
+import org.jasig.cas.CentralAuthenticationService;
 import org.jasig.cas.authentication.principal.UsernamePasswordCredentials;
 import org.jasig.cas.web.support.WebUtils;
+import org.jasig.cas.ticket.registry.TicketRegistry;
+import org.jasig.cas.ticket.Ticket;
 
 public final class CheckInt {
 	
@@ -15,19 +18,27 @@ public final class CheckInt {
 	
 	private List<String> reqLogin;
 	
+	/** Instance of CentralAuthenticationService. */
+    @NotNull
+    private CentralAuthenticationService centralAuthenticationService;
+	
 	public  String check( RequestContext context,  UsernamePasswordCredentials credentials) throws Exception {
 		
-		 HttpServletRequest request = WebUtils.getHttpServletRequest(context);
+		HttpServletRequest request = WebUtils.getHttpServletRequest(context);
 		
 		String interrupt = request.getParameter(CONST_PARAM_INTERRUPT);
 		
 		context.getFlowScope().put("interrupt", interrupt);	
 		
+		final String ticketGrantingTicketId = WebUtils.getTicketGrantingTicketId(context);
+		
+		Ticket ticketGrantingTicket = centralAuthenticationService.ticketRegistry.getTicket(ticketGrantingTicketId);
+		
 		if (interrupt == null) {
 			return "noInterrupt";
 		}	
 		if (reqLogin.indexOf(interrupt) != -1){
-			if (credentials != null && credentials.getUsername() != null) {
+			if (!ticketGrantingTicket.isExpired()) {
 				return "LoginOk";
 			} else {
 				return "LoginReq";
@@ -36,6 +47,12 @@ public final class CheckInt {
 		return "LoginOk";
 
 	}
+	
+	public void setCentralAuthenticationService(
+        final CentralAuthenticationService centralAuthenticationService) {
+        this.centralAuthenticationService = centralAuthenticationService;
+    }
+	
 	
 	public final void setReqLogin(final List<String> reqLogin) {
 	        this.reqLogin = reqLogin;
