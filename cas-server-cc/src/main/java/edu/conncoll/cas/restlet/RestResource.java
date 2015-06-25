8d88d970a436bc4eeeddac2f3c17b9c1c01253cf
Mike Matovic <mmatovic@conncoll.edu>
2015-06-25 14:55:29 -0400

    added options handling

--- cas-server-cc/src/main/java/edu/conncoll/cas/restlet/RestResource.java
+++ cas-server-cc/src/main/java/edu/conncoll/cas/restlet/RestResource.java
@@ -11,6 +11,7 @@ import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.json.JSONException;
 import org.json.JSONObject;
+import org.restlet.data.Form;
 import org.restlet.data.MediaType;
 import org.restlet.data.Status;
 import org.restlet.ext.json.JsonRepresentation;
@@ -77,6 +78,17 @@ public class RestResource extends Resource
 	}
 	
 	private Log log = LogFactory.getLog(this.getClass());
+	
+	@Override
+	public void handleOptions() {
+		Form headers = (Form)getResponse().getAttributes().get("org.restlet.http.headers");
+		if ( headers == null ) {
+			headers = new Form();
+			getResponse().getAttributes().put("org.restlet.http.headers", headers);
+		}
+		headers.add("Access-Control-Allow-Origin","https://cameldev.conncoll.edu");
+		super.handleOptions();
+	}
 	
 	@Override
 	public void acceptRepresentation( Representation resetEntity ) {
