92dd0e47f335f60c682356eb07d44f574aed391d
Mike Matovic <mmatovic@conncoll.edu>
2015-06-29 13:19:25 -0400

    added accepted domains array list

--- cas-server-cc/src/main/java/edu/conncoll/cas/restlet/RestHeader.java
+++ cas-server-cc/src/main/java/edu/conncoll/cas/restlet/RestHeader.java
@@ -1,5 +1,9 @@
 package edu.conncoll.cas.restlet;
 
+import java.util.ArrayList;
+
+import javax.validation.constraints.NotNull;
+
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.restlet.Filter;
@@ -22,6 +26,9 @@ import org.restlet.data.Response;
  */
 public class RestHeader extends Filter{
 	
+	@NotNull
+	private ArrayList<String> acceptedDomains;
+	
 	private Log log = LogFactory.getLog(this.getClass());
 	
 	public RestHeader() {
@@ -32,12 +39,21 @@ public class RestHeader extends Filter{
 	@Override
 	protected int beforeHandle(Request request, Response response) {
 		Form headers = (Form)response.getAttributes().get("org.restlet.http.headers");
+		Form requestHeaders = (Form)request.getAttributes().get("org.restlet.http.headers");
+		String responseHeader = "";
 		
 		if ( headers == null ) {
 			headers = new Form();
 			response.getAttributes().put("org.restlet.http.headers", headers);
 		}
-		headers.add("Access-Control-Allow-Origin","https://cameldev.conncoll.edu");
+		
+		for ( String acceptedDomain : acceptedDomains ) {
+			if ( requestHeaders.contains( acceptedDomain ) ) {
+				responseHeader = acceptedDomain;
+			}
+		}
+		
+		headers.add("Access-Control-Allow-Origin",responseHeader);
 		headers.add("Access-Control-Allow-Headers","Content-Type, Accept, Origin, Accept-Language, Accept-Encoding, User-Agent, Referer, Host");
 		log.debug("Access-Control-Allow headers added to response");
 		return super.beforeHandle(request, response);
